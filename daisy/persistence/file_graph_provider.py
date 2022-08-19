b7678eb26c23077db4f5b64775463885006d6e9a
Tri M. Nguyen <tri_nguyen@hms.harvard.edu>
2022-08-19 15:40:35 -0400

    Improving file graph

--- daisy/persistence/file_graph_provider.py
+++ daisy/persistence/file_graph_provider.py
@@ -29,6 +29,10 @@ class FileGraphProvider(SharedGraphProvider):
 
             The path to the graph container directory.
 
+        chunk_size (list-like of ``int``s):
+
+            Chunk size of each file in the container.
+
         mode (``string``, optional):
 
             One of ``r``, ``r+``, or ``w``. Defaults to ``r+``. ``w`` drops the
@@ -37,6 +41,11 @@ class FileGraphProvider(SharedGraphProvider):
         directed (``bool``):
 
             True if the graph is directed, false otherwise
+
+        roi_offset (list-like of ``int``s):
+
+            Offset of the first chunk in the container. Useful when ROI
+            does not align to `chunk_size`.
 
         nodes_collection (``string``):
         edges_collection (``string``):
@@ -51,6 +60,33 @@ class FileGraphProvider(SharedGraphProvider):
             string, the attribute is assumed to be an array. If a list, each
             entry denotes the position coordinates in order (e.g.,
             `position_z`, `position_y`, `position_x`).
+
+        save_attributes_as_single_file (``bool``, default ``False``):
+
+            Whether to save attributes as a single file to increase storage
+            efficiency or as separate files one for each attribute.
+
+        nodes_chunk_size (list-like of ``int``s):
+        edges_chunk_size (list-like of ``int``s):
+
+            Allowing chunk size to be set separately for nodes and edges.
+
+        nodes_roi_offset (list-like of ``int``s):
+        edges_roi_offset (list-like of ``int``s):
+
+            Allowing offset to be set separately for nodes and edges.
+
+        nodes_no_misaligned_reads (``bool``, default ``False``):
+        nodes_no_misaligned_writes (``bool``, default ``False``):
+        edges_no_misaligned_reads (``bool``, default ``False``):
+        edges_no_misaligned_writes (``bool``, default ``False``):
+
+            Disallow misaligned chunk access. Useful as error assertions.
+
+        nodes_no_filter_misaligned_reads (``bool``, default ``False``):
+
+            When reads are misaligned, return all nodes within the chunk
+            instead of filtering out nodes outside of the requested ROI.
     '''
 
     def __init__(
@@ -60,9 +96,21 @@ class FileGraphProvider(SharedGraphProvider):
             mode='r+',
             directed=None,
             total_roi=None,
+            roi_offset=None,
             nodes_collection='nodes',
             edges_collection='edges',
-            position_attribute='position'):
+            position_attribute='position',
+            save_attributes_as_single_file=False,
+            nodes_chunk_size=None,
+            edges_chunk_size=None,
+            nodes_roi_offset=None,
+            edges_roi_offset=None,
+            nodes_no_misaligned_reads=False,
+            nodes_no_misaligned_writes=False,
+            edges_no_misaligned_reads=False,
+            edges_no_misaligned_writes=False,
+            nodes_no_filter_misaligned_reads=False,
+            ):
 
         self.directory = directory
         self.chunk_size = Coordinate(chunk_size)
@@ -72,6 +120,30 @@ class FileGraphProvider(SharedGraphProvider):
         self.nodes_collection_name = nodes_collection
         self.edges_collection_name = edges_collection
         self.position_attribute = position_attribute
+        self.save_attributes_as_single_file = save_attributes_as_single_file
+        self.nodes_no_misaligned_reads = nodes_no_misaligned_reads
+        self.nodes_no_misaligned_writes = nodes_no_misaligned_writes
+        self.edges_no_misaligned_reads = edges_no_misaligned_reads
+        self.edges_no_misaligned_writes = edges_no_misaligned_writes
+        self.nodes_no_filter_misaligned_reads = nodes_no_filter_misaligned_reads
+
+        if roi_offset is None:
+            roi_offset = ((0,)*len(chunk_size))
+        self.roi_offset = Coordinate(roi_offset)
+
+        self.nodes_chunk_size = chunk_size
+        self.edges_chunk_size = chunk_size
+        if nodes_chunk_size is not None:
+            self.nodes_chunk_size = Coordinate(nodes_chunk_size)
+        if edges_chunk_size is not None:
+            self.edges_chunk_size = Coordinate(edges_chunk_size)
+
+        self.nodes_roi_offset = roi_offset
+        self.edges_roi_offset = roi_offset
+        if nodes_roi_offset is not None:
+            self.nodes_roi_offset = Coordinate(nodes_roi_offset)
+        if edges_roi_offset is not None:
+            self.edges_roi_offset = Coordinate(edges_roi_offset)
 
         self.nodes_collection = os.path.join(
             self.directory,
@@ -105,12 +177,13 @@ class FileGraphProvider(SharedGraphProvider):
         else:
             self.__set_metadata()
 
-    def get_chunks(self, roi):
+    def get_chunks(self, roi, chunk_size, roi_offset):
         '''Get a list of chunk indices and a list of chunk ROIs for each chunk
         that overlaps with the given ROI.'''
 
-        chunk_roi = roi.snap_to_grid(self.chunk_size, mode='grow')
-        chunks = chunk_roi/self.chunk_size
+        roi = roi.shift(-roi_offset)
+        chunk_roi = roi.snap_to_grid(chunk_size, mode='grow')
+        chunks = chunk_roi/chunk_size
 
         chunk_indices = itertools.product(*[
             range(chunks.get_begin()[d], chunks.get_end()[d])
@@ -157,8 +230,9 @@ class FileGraphProvider(SharedGraphProvider):
 
     def _write_nodes_to_chunk(self, chunk_index, nodes, roi=None):
 
-        chunk_roi = Roi(chunk_index, (1,)*self.chunk_size.dims)
-        chunk_roi *= self.chunk_size
+        chunk_roi = Roi(chunk_index, (1,)*self.nodes_chunk_size.dims)
+        chunk_roi *= self.nodes_chunk_size
+        chunk_roi += self.nodes_roi_offset
 
         path = self.__chunk_nodes_path(chunk_index)
         os.makedirs(path, exist_ok=True)
@@ -167,13 +241,18 @@ class FileGraphProvider(SharedGraphProvider):
             attributes = list(nodes.keys())
             json.dump({'attributes': attributes}, f)
 
-        if roi is not None and not roi.contains(chunk_roi):
+        if roi is not None and not roi.contains(chunk_roi) and \
+                not chunk_roi.contains(roi):
 
             roi_filter = self.__get_roi_filter(nodes, roi)
             for k, v in nodes.items():
                 nodes[k] = list(np.array(nodes[k])[roi_filter])
-        for k, v in nodes.items():
-            np.savez_compressed(os.path.join(path, k), nodes=v)
+
+        if self.save_attributes_as_single_file:
+            np.savez_compressed(os.path.join(path, 'nodes'), **nodes)
+        else:
+            for k, v in nodes.items():
+                np.savez_compressed(os.path.join(path, k), nodes=v)
 
     def _write_edges_to_chunk(
             self,
@@ -182,8 +261,9 @@ class FileGraphProvider(SharedGraphProvider):
             edge_positions,
             roi=None):
 
-        chunk_roi = Roi(chunk_index, (1,)*self.chunk_size.dims)
-        chunk_roi *= self.chunk_size
+        chunk_roi = Roi(chunk_index, (1,)*self.edges_chunk_size.dims)
+        chunk_roi *= self.edges_chunk_size
+        chunk_roi += self.edges_roi_offset
 
         path = self.__chunk_edges_path(chunk_index)
         os.makedirs(path, exist_ok=True)
@@ -192,7 +272,8 @@ class FileGraphProvider(SharedGraphProvider):
             attributes = list(edges.keys())
             json.dump({'attributes': attributes}, f)
 
-        if roi is not None and not roi.contains(chunk_roi):
+        if roi is not None and not roi.contains(chunk_roi) and \
+                not chunk_roi.contains(roi):
 
             roi_filter = np.ones((len(edges),), dtype=np.bool)
             for d in range(roi.dims):
@@ -203,13 +284,17 @@ class FileGraphProvider(SharedGraphProvider):
             for k, v in edges.items():
                 edges[k] = edges[k][roi_filter]
 
-        for k, v in edges.items():
-            np.savez_compressed(os.path.join(path, k), edges=v)
+        if self.save_attributes_as_single_file:
+            np.savez_compressed(os.path.join(path, 'edges'), **edges)
+        else:
+            for k, v in edges.items():
+                np.savez_compressed(os.path.join(path, k), edges=v)
 
     def _read_nodes_from_chunk(self, chunk_index, roi=None):
 
-        chunk_roi = Roi(chunk_index, (1,)*self.chunk_size.dims)
-        chunk_roi *= self.chunk_size
+        chunk_roi = Roi(chunk_index, (1,)*self.nodes_chunk_size.dims)
+        chunk_roi *= self.nodes_chunk_size
+        chunk_roi += self.nodes_roi_offset
 
         path = self.__chunk_nodes_path(chunk_index)
         if not os.path.exists(path):
@@ -219,14 +304,23 @@ class FileGraphProvider(SharedGraphProvider):
             meta = json.load(f)
 
         nodes = {}
+
+        if self.save_attributes_as_single_file:
+            nodes_npz = np.load(os.path.join(path, 'nodes.npz'),
+                                allow_pickle=True)
+            for attribute in meta['attributes']:
+                nodes[attribute] = nodes_npz[attribute]
+
         for attribute in meta['attributes']:
             file_path = os.path.join(path, attribute + '.npz')
             if os.path.exists(file_path):
                 nodes[attribute] = np.load(file_path,
                                            allow_pickle=True)['nodes']
 
-        if roi is None or roi.contains(chunk_roi):
+        if roi is None or roi.contains(chunk_roi) or \
+                self.nodes_no_filter_misaligned_reads:
             return nodes
+
         roi_filter = self.__get_roi_filter(nodes, roi)
         for k, v in nodes.items():
             nodes[k] = nodes[k][roi_filter]
@@ -234,8 +328,9 @@ class FileGraphProvider(SharedGraphProvider):
 
     def _read_edges_from_chunk(self, chunk_index, roi=None, node_ids=None):
 
-        chunk_roi = Roi(chunk_index, (1,)*self.chunk_size.dims)
-        chunk_roi *= self.chunk_size
+        chunk_roi = Roi(chunk_index, (1,)*self.edges_chunk_size.dims)
+        chunk_roi *= self.edges_chunk_size
+        chunk_roi += self.edges_roi_offset
 
         path = self.__chunk_edges_path(chunk_index)
         if not os.path.exists(path):
@@ -245,11 +340,19 @@ class FileGraphProvider(SharedGraphProvider):
             meta = json.load(f)
 
         edges = {}
-        for attribute in meta['attributes']:
-            file_path = os.path.join(path, attribute + '.npz')
-            if os.path.exists(file_path):
-                edges[attribute] = np.load(file_path,
-                                           allow_pickle=True)['edges']
+
+        if self.save_attributes_as_single_file:
+            edges_npz = np.load(os.path.join(path, 'edges.npz'),
+                                allow_pickle=True)
+            for attribute in meta['attributes']:
+                edges[attribute] = edges_npz[attribute]
+
+        else:
+            for attribute in meta['attributes']:
+                file_path = os.path.join(path, attribute + '.npz')
+                if os.path.exists(file_path):
+                    edges[attribute] = np.load(file_path,
+                                               allow_pickle=True)['edges']
 
         # we assume that if the chunk is contained in ROI, there is no need to
         # filter for node_ids any more
@@ -267,7 +370,15 @@ class FileGraphProvider(SharedGraphProvider):
 
         nodes = {}
         logger.debug("Reading nodes in roi %s" % roi)
-        for chunk_index in self.get_chunks(roi):
+
+        if self.nodes_no_misaligned_reads:
+            assert roi.shift(-self.nodes_roi_offset).begin.is_multiple_of(
+                        self.nodes_chunk_size), "Misaligned nodes read " \
+                        f"to {roi} with chunks {self.nodes_chunk_size}"
+
+        for chunk_index in self.get_chunks(roi,
+                                           self.nodes_chunk_size,
+                                           self.nodes_roi_offset):
             logger.debug("Reading nodes in chunk %s" % str(chunk_index))
             chunk_nodes = self._read_nodes_from_chunk(chunk_index, roi)
             if len(chunk_nodes) == 0 or len(chunk_nodes['id']) == 0:
@@ -299,6 +410,11 @@ class FileGraphProvider(SharedGraphProvider):
     def read_edges(self, roi, nodes=None):
         '''Returns a list of edges within roi.'''
 
+        if self.edges_no_misaligned_reads:
+            assert roi.shift(-self.edges_roi_offset).begin.is_multiple_of(
+                        self.edges_chunk_size), "Misaligned edges read " \
+                        f"to {roi} with chunks {self.edges_chunk_size}"
+
         if nodes is None:
             nodes = self.read_nodes(roi)
 
@@ -306,7 +422,9 @@ class FileGraphProvider(SharedGraphProvider):
             return {}
 
         edges = {}
-        for chunk_index in self.get_chunks(roi):
+        for chunk_index in self.get_chunks(roi,
+                                           self.edges_chunk_size,
+                                           self.edges_roi_offset):
 
             chunk_edges = self._read_edges_from_chunk(
                 chunk_index,
@@ -327,14 +445,32 @@ class FileGraphProvider(SharedGraphProvider):
 
     def __getitem__(self, roi):
 
-        # get all nodes within roi
-        nodes = self.read_nodes(roi)
-        edges = self.read_edges(roi, nodes)
+        return self.get_graph(roi, empty_init=False, no_edges_init=False)
+
+    def get_graph(
+            self,
+            roi,
+            empty_init=False,
+            no_edges_init=False):
 
         if self.directed:
-            graph = FileSubDiGraph(self, roi)
+            graph = FileSubDiGraph(
+                self, roi,
+                self.nodes_chunk_size, self.edges_chunk_size,
+                self.nodes_roi_offset, self.edges_roi_offset,
+                )
         else:
-            graph = FileSubGraph(self, roi)
+            graph = FileSubGraph(
+                self, roi,
+                self.nodes_chunk_size, self.edges_chunk_size,
+                self.nodes_roi_offset, self.edges_roi_offset,
+                )
+
+        if empty_init:
+            return graph
+
+        # get all nodes within roi
+        nodes = self.read_nodes(roi)
 
         if len(nodes) > 0:
             node_list = [
@@ -349,6 +485,11 @@ class FileGraphProvider(SharedGraphProvider):
                 for i in range(len(nodes['id']))
             ]
             graph.add_nodes_from(node_list)
+
+        if no_edges_init:
+            return graph
+
+        edges = self.read_edges(roi, nodes)
 
         if len(edges) > 0:
             edge_list = [
@@ -425,13 +566,22 @@ class FileSharedSubGraph(SharedSubGraph):
     def __init__(
             self,
             graph_provider,
-            roi):
+            roi,
+            nodes_chunk_size,
+            edges_chunk_size,
+            nodes_roi_offset,
+            edges_roi_offset
+            ):
 
         super().__init__()
 
         self.graph_provider = graph_provider
         self.roi = roi
         self.pos_list = type(self.graph_provider.position_attribute) == list
+        self.nodes_chunk_size = nodes_chunk_size
+        self.edges_chunk_size = edges_chunk_size
+        self.nodes_roi_offset = nodes_roi_offset
+        self.edges_roi_offset = edges_roi_offset
 
     def __get_node_pos(self, n):
 
@@ -460,7 +610,7 @@ class FileSharedSubGraph(SharedSubGraph):
             fail_if_not_exists=False,
             delete=False):
         assert not delete, "Delete not implemented"
-        assert not (fail_if_exists and fail_if_not_exists),\
+        assert not(fail_if_exists and fail_if_not_exists),\
             "Cannot have fail_if_exists and fail_if_not_exists simultaneously"
         if fail_if_exists:
             raise RuntimeError("Fail if exists not implemented for "
@@ -476,6 +626,11 @@ class FileSharedSubGraph(SharedSubGraph):
             roi = self.roi
 
         logger.debug("Writing edges in %s", roi)
+
+        if self.graph_provider.edges_no_misaligned_writes:
+            assert roi.shift(-self.edges_roi_offset).begin.is_multiple_of(
+                        self.edges_chunk_size), "Misaligned edges write " \
+                        f"to {roi} with chunks {self.edges_chunk_size}"
 
         edges = {'u': [], 'v': []}
         edge_positions = []
@@ -503,7 +658,9 @@ class FileSharedSubGraph(SharedSubGraph):
         for k, v in edges.items():
             v += [None]*(num_entries - len(v))
 
-        for chunk in self.graph_provider.get_chunks(roi):
+        for chunk in self.graph_provider.get_chunks(roi,
+                                                    self.edges_chunk_size,
+                                                    self.edges_roi_offset):
             self.graph_provider._write_edges_to_chunk(chunk, edges, roi)
 
     def write_nodes(
@@ -514,7 +671,7 @@ class FileSharedSubGraph(SharedSubGraph):
             fail_if_not_exists=False,
             delete=False):
         assert not delete, "Delete not implemented"
-        assert not (fail_if_exists and fail_if_not_exists),\
+        assert not(fail_if_exists and fail_if_not_exists),\
             "Cannot have fail_if_exists and fail_if_not_exists simultaneously"
         if fail_if_exists:
             raise RuntimeError("Fail if exists not implemented for "
@@ -531,6 +688,11 @@ class FileSharedSubGraph(SharedSubGraph):
             roi = self.roi
 
         logger.debug("Writing nodes in %s", roi)
+
+        if self.graph_provider.nodes_no_misaligned_writes:
+            assert roi.shift(-self.nodes_roi_offset).begin.is_multiple_of(
+                        self.nodes_chunk_size), "Misaligned nodes write " \
+                        f"to {roi} with chunks {self.nodes_chunk_size}"
 
         nodes = {'id': []}
         for n, data in self.nodes(data=True):
@@ -554,7 +716,9 @@ class FileSharedSubGraph(SharedSubGraph):
         for k, v in nodes.items():
             v += [None]*(num_entries - len(v))
 
-        for chunk in self.graph_provider.get_chunks(roi):
+        for chunk in self.graph_provider.get_chunks(roi,
+                                                    self.nodes_chunk_size,
+                                                    self.nodes_roi_offset):
             self.graph_provider._write_nodes_to_chunk(chunk, nodes, roi)
 
     def __contains(self, roi, node):
@@ -579,12 +743,20 @@ class FileSubGraph(FileSharedSubGraph, Graph):
     def __init__(
             self,
             graph_provider,
-            roi):
+            roi,
+            nodes_chunk_size,
+            edges_chunk_size,
+            nodes_roi_offset,
+            edges_roi_offset):
         # this calls the init function of the FileSharedSubGraph,
         # because left parents come before right parents
         super().__init__(
                 graph_provider,
-                roi)
+                roi,
+                nodes_chunk_size,
+                edges_chunk_size,
+                nodes_roi_offset,
+                edges_roi_offset)
 
     def is_directed(self):
         return False
@@ -594,12 +766,20 @@ class FileSubDiGraph(FileSharedSubGraph, DiGraph):
     def __init__(
             self,
             graph_provider,
-            roi):
+            roi,
+            nodes_chunk_size,
+            edges_chunk_size,
+            nodes_roi_offset,
+            edges_roi_offset):
         # this calls the init function of the FileSharedSubGraph,
         # because left parents come before right parents
         super().__init__(
                 graph_provider,
-                roi)
+                roi,
+                nodes_chunk_size,
+                edges_chunk_size,
+                nodes_roi_offset,
+                edges_roi_offset)
 
     def is_directed(self):
         return True
